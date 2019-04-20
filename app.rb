5bae029e4d72b17837a827fed06643c0d3e6a896
Heather Kirk <hskirk@gmail.com>
2019-04-20 23:19:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,18 +7,21 @@ class App < Sinatra::Base
  @name = params[:name].reverse
  end
  
- get 'square/:number' do 
-   @square = (params[:number].to_i ** 2).to_s
-   binding.pry
- end 
- 
- get 'say/:number/:phrase' do 
-   
-   
+ get '/square/:number' do 
+   (params[:number].to_i ** 2).to_s
    
  end 
  
- get 'say/:word1/:word2/:word3/:word4/:word5' do
+ get '/say/:number/:phrase' do 
+   @phrase = ''
+   
+   params[:number].to_i.times do 
+     @phrase += params[:phrase]
+   end
+   @phrase
+ end 
+ 
+ get '/say/:word1/:word2/:word3/:word4/:word5' do
  
    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
  end 

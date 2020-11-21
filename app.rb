559cd2e77d325c0e7198f40f73492028b39c2d40
Yumacorn <ReachRyanMui@gmail.com>
2020-11-20 21:23:40 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,23 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name]
+    @name = @name.reverse
+  end
 
+  get '/square/:number' do
+    @number = params[:number]
+    "#{@number.to_i * @number.to_i}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
+    string =""
+    @number.to_i.times do
+      string =+ "#{@phrase}"
+    end
+    string
+  end
 end

82ee213a765c821e1b4ab3b75dd4369a0848bccb
Yumacorn <ReachRyanMui@gmail.com>
2020-11-21 08:38:58 -0500

    Done.

--- app.rb
+++ app.rb
@@ -17,8 +17,35 @@ class App < Sinatra::Base
     @phrase = params[:phrase]
     string =""
     @number.to_i.times do
-      string =+ "#{@phrase}"
+      string << "#{@phrase}"
     end
     string
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
+    string =""
+    string = "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @a = params[:number1].to_i
+    @b = params[:number2].to_i
+    result = 0
+    if @operation == "add"
+      result = @a + @b
+    elsif @operation == "subtract"
+      result = @a - @b
+    elsif @operation == "multiply"
+      result = @a * @b
+    elsif @operation == "divide"
+      result = @a / @b
+    end
+    "#{result}"
   end
 end
